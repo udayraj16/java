cd956871b5a92e3cf684a61c07bfdede96d0cde0
udayraj16 <uday17416@gmail.com>
2025-12-10 21:02:40 +0530

    first commit

---
+++ main.java
@@ -0,0 +1,9 @@
+import java.util.Date;
+
+public class main {
+
+    public static void main(String[] args) {
+        System.out.println("hello ");
+        System.out.println(new Date());
+    }
+}
